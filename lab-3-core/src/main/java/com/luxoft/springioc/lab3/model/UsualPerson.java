a481e5675ff896a0314f12c46a5a6baea386ecfd
Irina Ivlieva <ivlieva@haulmont.com>
2021-02-07 18:57:35 +0400

    lab 3. Part 1

--- lab-3-core/src/main/java/com/luxoft/springioc/lab3/model/UsualPerson.java
+++ lab-3-core/src/main/java/com/luxoft/springioc/lab3/model/UsualPerson.java
@@ -1,36 +1,34 @@
 package com.luxoft.springioc.lab3.model;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import java.util.List;
 
+@Service("person")
 //@Component("person")
 public class UsualPerson implements Person {
-	
-	public static int createdPersons = 0; 
 
-//    @Value("${person.id}")
+    public static int createdPersons = 0;
+
+    @Value("${person.id}")
     private int id;
-
+    @Value("${person.name}")
     private String name;
 
     @Autowired
     private Country country;
-
+    @Value("${person.age}")
     private int age;
-
+    @Value("${person.height}")
     private float height;
-
+    @Value("${person.isProgrammer}")
     private boolean isProgrammer;
-
+    @Value("${person.isRegistered}")
     private boolean isRegistered;
 
-	private List<String> contacts;
+    private List<String> contacts;
 
     public void setIsProgrammer(boolean isProgrammer) {
         this.isProgrammer = isProgrammer;
@@ -44,12 +42,12 @@ public class UsualPerson implements Person {
         this.height = height;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void sayHello(Person person) {
@@ -70,14 +68,14 @@ public class UsualPerson implements Person {
     public void setProgrammer(boolean programmer) {
         isProgrammer = programmer;
     }
-    
-    public boolean isRegistered() {
-		return isRegistered;
-	}
 
-	public void setRegistered(boolean isRegistered) {
-		this.isRegistered = isRegistered;
-	}
+    public boolean isRegistered() {
+        return isRegistered;
+    }
+
+    public void setRegistered(boolean isRegistered) {
+        this.isRegistered = isRegistered;
+    }
 
     public List<String> getContacts() {
         return contacts;
